87424bd8f0739f7b9112d2ee3d302e18516f645f
MrLepoischiche <esteban-jean.pro@hotmail.com>
2025-08-30 18:23:57 +0200

    `r#move` = `update_position` + `render` & Vehicle automatically advances once created & Methods now use constants for vehicles' width and height

--- src/simulation/vehicle.rs
+++ src/simulation/vehicle.rs
@@ -1,6 +1,6 @@
 use sdl2;
 
-use crate::config::VEHICLE_SPEED;
+use crate::config::{VEHICLE_HEIGHT, VEHICLE_SPEED, VEHICLE_WIDTH};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum VehicleDirection {
@@ -23,33 +23,58 @@ impl<'a> Vehicle {
         id: i32,
         position: (f32, f32),
         target: (f32, f32),
-        speed: u16,
         direction: VehicleDirection,
     ) -> Self {
         Vehicle {
             id,
             position,
             target,
-            speed,
+            speed: VEHICLE_SPEED,
             direction,
         }
     }
 
+    pub fn render(
+        &self,
+        canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    ) {
+        let rect = match self.direction {
+            VehicleDirection::North | VehicleDirection::South => sdl2::rect::Rect::new(
+                self.position.0 as i32 - (VEHICLE_WIDTH / 2) as i32,
+                self.position.1 as i32 - (VEHICLE_HEIGHT / 2) as i32,
+                VEHICLE_WIDTH,
+                VEHICLE_HEIGHT,
+            ),
+            VehicleDirection::East | VehicleDirection::West => sdl2::rect::Rect::new(
+                self.position.0 as i32 - (VEHICLE_HEIGHT / 2) as i32,
+                self.position.1 as i32 - (VEHICLE_WIDTH / 2) as i32,
+                VEHICLE_HEIGHT,
+                VEHICLE_WIDTH,
+            ),
+        };
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 255));
+        let _ = canvas.fill_rect(rect);
+    }
+
     // delta_time = 60 fps
-    pub fn update_position(&mut self) {
-        let direction: (f32, f32) = (
-            self.target.0 - self.position.0,
-            self.target.1 - self.position.1,
-        );
-        let distance = (direction.0.powi(2) + direction.1.powi(2)).sqrt();
+    pub fn r#move(&mut self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
+        if self.speed > 0 {
+            let direction: (f32, f32) = (
+                self.target.0 - self.position.0,
+                self.target.1 - self.position.1,
+            );
+            let distance = (direction.0.powi(2) + direction.1.powi(2)).sqrt();
 
-        if distance > 0.0 {
-            let normalized_x = direction.0 / distance;
-            let normalized_y = direction.1 / distance;
+            if distance > 0.0 {
+                let normalized_x = direction.0 / distance;
+                let normalized_y = direction.1 / distance;
 
-            self.position.0 += normalized_x * self.speed as f32 * 1.0 / 60.0;
-            self.position.1 += normalized_y * self.speed as f32 * 1.0 / 60.0;
+                self.position.0 += normalized_x * self.speed as f32 * 1.0 / 60.0;
+                self.position.1 += normalized_y * self.speed as f32 * 1.0 / 60.0;
+            }
         }
+
+        self.render(canvas);
     }
 
     pub fn update_target(&mut self, target: (f32, f32)) {
@@ -87,7 +112,6 @@ impl<'a> Vehicle {
     pub fn find_closest_vehicle_ahead(
         &'a self,
         vehicles: &'a [Vehicle],
-        vehicle_width: u32,
     ) -> Option<&'a Vehicle> {
         vehicles
             .iter()
@@ -95,19 +119,19 @@ impl<'a> Vehicle {
             .filter(|&other| match self.direction {
                 VehicleDirection::North => {
                     other.position.1 < self.position.1
-                        && (other.position.0 - self.position.0).abs() < vehicle_width as f32
+                        && (other.position.0 - self.position.0).abs() < VEHICLE_WIDTH as f32
                 }
                 VehicleDirection::South => {
                     other.position.1 > self.position.1
-                        && (other.position.0 - self.position.0).abs() < vehicle_width as f32
+                        && (other.position.0 - self.position.0).abs() < VEHICLE_WIDTH as f32
                 }
                 VehicleDirection::East => {
                     other.position.0 > self.position.0
-                        && (other.position.1 - self.position.1).abs() < vehicle_width as f32
+                        && (other.position.1 - self.position.1).abs() < VEHICLE_WIDTH as f32
                 }
                 VehicleDirection::West => {
                     other.position.0 < self.position.0
-                        && (other.position.1 - self.position.1).abs() < vehicle_width as f32
+                        && (other.position.1 - self.position.1).abs() < VEHICLE_WIDTH as f32
                 }
             })
             .min_by(|&a, &b| {
@@ -119,30 +143,6 @@ impl<'a> Vehicle {
                 .sqrt();
                 dist_a.partial_cmp(&dist_b).unwrap()
             })
-    }
-
-    pub fn render(
-        &self,
-        canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-        vehicle_width: u32,
-        vehicle_height: u32,
-    ) {
-        let rect = match self.direction {
-            VehicleDirection::North | VehicleDirection::South => sdl2::rect::Rect::new(
-                self.position.0 as i32 - (vehicle_width / 2) as i32,
-                self.position.1 as i32 - (vehicle_height / 2) as i32,
-                vehicle_width,
-                vehicle_height,
-            ),
-            VehicleDirection::East | VehicleDirection::West => sdl2::rect::Rect::new(
-                self.position.0 as i32 - (vehicle_height / 2) as i32,
-                self.position.1 as i32 - (vehicle_width / 2) as i32,
-                vehicle_height,
-                vehicle_width,
-            ),
-        };
-        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 255));
-        let _ = canvas.fill_rect(rect);
     }
 }
 
