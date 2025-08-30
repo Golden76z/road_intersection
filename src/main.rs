25155abf38882d44e9a0227043d556550dfd618f
Theo Ichane <ichane.theo@gmail.com>
2025-08-30 18:22:40 +0200

    adding traffic lights to map and method change_state

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // extern crate sdl2;
 
+use std::collections::HashMap;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
+use sdl2::rect::Rect;
 
 mod input;
 mod render;
@@ -32,6 +34,7 @@ pub fn main() {
     // canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     // let mut i = 0;
+
     'running: loop {
         // i = (i + 1) % 255;
         // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
@@ -43,11 +46,29 @@ pub fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::N),
+                    ..
+                } => renderer.change_state("North"),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    ..
+                } => renderer.change_state("West"),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    ..
+                } => renderer.change_state("South"),
+
+                Event::KeyDown {
+                    keycode: Some(Keycode::E),
+                    ..
+                } => renderer.change_state("East"),
                 _ => {}
             }
         }
         // The rest of the game loop goes here...
-
         renderer.draw().unwrap();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
--- src/render/sdl_renderer.rs
+++ src/render/sdl_renderer.rs
@@ -1,44 +1,45 @@
 use std::cmp::{max, min};
+use std::collections::{HashMap};
 use sdl2::pixels::Color;
-use sdl2::rect;
 use sdl2::rect::{Rect, Point};
 use sdl2::render::WindowCanvas;
 use sdl2::video::Window;
 use crate::config::*;
+use crate::simulation::{TrafficLight};
 
-pub struct Renderer { canvas: WindowCanvas }
+pub struct Renderer { pub canvas: WindowCanvas, pub lights: HashMap<String,TrafficLight> }
 
 impl Renderer {
     pub fn new(window: Window ) -> Result<Renderer, String> {
         let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-        Ok(Renderer { canvas })
+        let lights :HashMap<String,TrafficLight> = HashMap::from([
+            (String::from("North"),TrafficLight::new(Rect::new((GRID_WIDTH_PIXELS/2 - 2 * CASE_SIZE -1).cast_signed(), (GRID_HEIGHT_PIXELS/2 - 2 * CASE_SIZE - 1).cast_signed(),CASE_SIZE, CASE_SIZE ))),
+            (String::from("West"),TrafficLight::new(Rect::new((GRID_WIDTH_PIXELS/2 + CASE_SIZE +1).cast_signed(), (GRID_HEIGHT_PIXELS/2 - 2 * CASE_SIZE -1).cast_signed(),CASE_SIZE, CASE_SIZE ))),
+            (String::from("East"),TrafficLight::new(Rect::new((GRID_WIDTH_PIXELS/2 - 2 * CASE_SIZE -1).cast_signed(), (GRID_HEIGHT_PIXELS/2 + CASE_SIZE +1).cast_signed(),CASE_SIZE, CASE_SIZE ))),
+            (String::from("South"),TrafficLight::new(Rect::new((GRID_WIDTH_PIXELS/2 + CASE_SIZE +1).cast_signed(), (GRID_HEIGHT_PIXELS/2 + CASE_SIZE +1).cast_signed(),CASE_SIZE, CASE_SIZE )))
+        
+        ]);
+        Ok(Renderer { canvas, lights })
     }
-
-    fn draw_dot(&mut self, point: &Point) -> Result<(), String> {
-        let x = point.x.cast_unsigned();
-        let y = point.y.cast_unsigned();
-        self.canvas.fill_rect(Rect::new(
-            (x * CASE_SIZE).cast_signed() ,
-            (y * CASE_SIZE).cast_signed(),
-            CASE_SIZE,
-            CASE_SIZE,
-        ))?;
-
-        Ok(())
+    //Lights
+    pub fn change_state(&mut self, s: &str) {
+        if let Some(light) = self.lights.get_mut(s) {
+            light.change_state();
+        }
     }
 
     //Only for straight line
     fn draw_dotted_line(&mut self, start: (i32,i32), end: (i32,i32)) -> Result<(), String> {
-        self.canvas.set_draw_color(Color::GREY);
+        self.canvas.set_draw_color(Color::WHITE);
         match start.0 == end.0 {
             true => {
                 let x = start.0;
                 let mut min = min(start.1,end.1);
                 let max = max(start.1, end.1);
-                let mut i = min +5;
+                let mut i = min +10;
                 while i < max {
                     self.canvas.draw_line(Point::new(x,min),Point::new(x,i))?;
-                    (min,i) = (i+10,i+15)
+                    (min,i) = (i+10,i+20)
                 }
                 if min < max {
                     self.canvas.draw_line(Point::new(x,min),Point::new(x,max))?;
@@ -49,10 +50,10 @@ impl Renderer {
                 let y = start.1;
                 let mut min = min(start.0,end.0);
                 let max = max(start.0, end.0);
-                let mut i = min +5;
+                let mut i = min +10;
                 while i < max {
                     self.canvas.draw_line(Point::new(min,y),Point::new(i,y))?;
-                    (min,i) = (i+10,i+15)
+                    (min,i) = (i+10,i+20)
                 }
                 if min < max {
                     self.canvas.draw_line(Point::new(min,y),Point::new(max,y))?;
@@ -61,7 +62,7 @@ impl Renderer {
             }
         }
     }
-    pub fn init_map(&mut self) -> Result<(), String> {
+    fn init_map(&mut self) -> Result<(), String> {
         self.canvas.set_draw_color(Color::BLACK);
         self.canvas.clear();
 
@@ -77,16 +78,16 @@ impl Renderer {
         self.draw_dotted_line((GRID_WIDTH_PIXELS.cast_signed(),(GRID_HEIGHT_PIXELS/2).cast_signed()),((GRID_WIDTH_PIXELS /2 + CASE_SIZE).cast_signed(),(GRID_HEIGHT_PIXELS/2).cast_signed()))?;
         self.draw_dotted_line(((GRID_WIDTH_PIXELS/2).cast_signed(),GRID_HEIGHT_PIXELS.cast_signed()),((GRID_WIDTH_PIXELS/2 ).cast_signed(),(GRID_HEIGHT_PIXELS/2 + CASE_SIZE).cast_signed()))?;
 
-
-
         Ok(())
     }
 
     pub fn draw(&mut self) -> Result<(), String> {
         self.init_map()?;
+        for light in self.lights.values_mut() {
+            light.draw(&mut self.canvas)?;
+        }
 
         self.canvas.present();
-
         Ok(())
     }
 }
